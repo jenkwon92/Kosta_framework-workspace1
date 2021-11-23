fd5941bbb81f844b61df97cccb9dc362c85ef0d5
jenkwon92 <silverkay92@gmail.com>
2021-11-23 17:27:42 +0900

    211123

--- 3-MyBatis-Study1-ex/src/model/AccountDAO.java
+++ 3-MyBatis-Study1-ex/src/model/AccountDAO.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.List;
-
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
@@ -9,7 +7,25 @@ import factory.SqlSessionFactoryBean;
 
 public class AccountDAO {
 	private static AccountDAO instance=new AccountDAO();
-	
+	private SqlSessionFactory factory;
+	private AccountDAO() {
+		factory=SqlSessionFactoryBean.getInstance().getObject();
+	}
+	public static AccountDAO getInstance(){
+		return instance;
+	}
+	public AccountVO findAccountById(String id) {
+		AccountVO vo = null;
+		SqlSession session = null;
+		try {
+			session = factory.openSession(); //session을 받아옴
+			//selectOne 1.매개변수 mapper sql id, 2. 매개변수 검색조건
+			vo = session.selectOne("account.findAccountById",id); //첫번째는 xml의 sql 아이디, 두번째는 검색 조건 
+		}finally {
+			session.close();
+		}
+		return vo;
+	}
 }
 
 
