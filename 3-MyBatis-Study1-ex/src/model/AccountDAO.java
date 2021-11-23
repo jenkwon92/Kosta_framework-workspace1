900e6993e08f51c15e7895767757c164f58cd89e
jenkwon92 <silverkay92@gmail.com>
2021-11-23 23:51:34 +0900

    211123

--- 3-MyBatis-Study1-ex/src/model/AccountDAO.java
+++ 3-MyBatis-Study1-ex/src/model/AccountDAO.java
@@ -1,46 +1,62 @@
 package model;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 import factory.SqlSessionFactoryBean;
 
 public class AccountDAO {
-	private static AccountDAO instance=new AccountDAO();
+	private static AccountDAO instance = new AccountDAO();
 	private SqlSessionFactory factory;
+
 	private AccountDAO() {
-		factory=SqlSessionFactoryBean.getInstance().getObject();
+		factory = SqlSessionFactoryBean.getInstance().getObject();
 	}
-	public static AccountDAO getInstance(){
+
+	public static AccountDAO getInstance() {
 		return instance;
 	}
+
 	public AccountVO findAccountById(String id) {
 		AccountVO vo = null;
 		SqlSession session = null;
 		try {
-			session = factory.openSession(); //session을 받아옴
-			//selectOne 1.매개변수 mapper sql id, 2. 매개변수 검색조건
-			vo = session.selectOne("account.findAccountById",id); //첫번째는 xml의 sql 아이디, 두번째는 검색 조건 
-		}finally {
+			session = factory.openSession(); // session을 받아옴
+			// selectOne 1.매개변수 mapper sql id, 2. 매개변수 검색조건
+			vo = session.selectOne("account.findAccountById", id); // 첫번째는 xml의 sql 아이디, 두번째는 검색 조건
+		} finally {
 			session.close();
 		}
 		return vo;
 	}
+
+	public void registerAccount(AccountVO vo) {
+		SqlSession session = null;
+		try {
+			// default ->autocommit false
+			// autocommit 설정 openSession(true)
+			session = factory.openSession();
+			// vo는 db에 insert 정보(pstmt.setXXX(1,id)....)
+			session.insert("account.registerAccount", vo);
+			session.commit(); // 수동커밋모드가 기본이므로 명시적으로 commit
+		} finally {
+			// 커밋되지 않으면 자동 롤백이 된다
+			session.close();
+		}
+	}
+
+	public List<AccountVO> getAllAccountList() {
+		List<AccountVO> list = null;
+		SqlSession session = null;
+		try {
+			session = factory.openSession();
+			list = session.selectList("account.getAllAccountList");
+		} finally {
+			session.close();
+		}
+		return list;
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- 3-MyBatis-Study1-ex/src/test/TestMybatis.java
+++ 3-MyBatis-Study1-ex/src/test/TestMybatis.java
@@ -7,7 +7,18 @@ import model.AccountVO;
 
 public class TestMybatis {
 	public static void main(String[] args) {
-		
+		AccountDAO dao = AccountDAO.getInstance();
+		//1. id로 계좌조회
+		//String id= "1";
+		//System.out.println("findAccountById "+dao.findAccountById(id));
+		//2.계좌등록
+		//AccountVO vo = new AccountVO ("2","강하늘",2000);
+		//dao.registerAccount(vo);
+		//System.out.println("registerAccount:"+dao.findAccountById("2"));
+		//3. 전체 계좌 조회
+		List<AccountVO> list = dao.getAllAccountList();
+		for(AccountVO vo:list)
+			System.out.println(vo);
 	}
 }
 
